590314ccaa52fc9ff9376c96d2989d2ec58fd3c5
STANISLAW <stas_dmitriev_2007@bk.ru>
2026-04-10 22:16:39 +0500

    Create INSERT.TB_PRODUCTS.sql

---
+++ TASKS/TASK21-DB2-FK-VALIDATION/SQL/INSERT.TB_PRODUCTS.sql
@@ -0,0 +1,8 @@
+-- TASK 21: INSERT SAMPLE DATA
+
+INSERT INTO TB_PRODUCTS (PROD_ID,PROD_NAME,UNIT_PRICE,STOCK_QTY)
+VALUES ('00100','LAPTOP PRO',50.00,1000);                                                                   
+INSERT INTO TB_PRODUCTS (PROD_ID,PROD_NAME,UNIT_PRICE,STOCK_QTY)
+VALUES ('00200','WIRELESS MOUSE',100.00,500);                                                                          
+INSERT INTO TB_PRODUCTS (PROD_ID,PROD_NAME,UNIT_PRICE,STOCK_QTY)
+VALUES ('00300','USB HUB',25.00,300);                           
